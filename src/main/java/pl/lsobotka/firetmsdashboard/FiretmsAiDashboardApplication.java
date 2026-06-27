c6521b80670a02010258af7809c609132d5304db
Łukasz Sobótka <l.sobotka@firetms.com>
2026-06-27 12:53:01 +0200

    Initialize Spring Boot Vaadin application

---
+++ src/main/java/pl/lsobotka/firetmsdashboard/FiretmsAiDashboardApplication.java
@@ -0,0 +1,12 @@
+package pl.lsobotka.firetmsdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiretmsAiDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiretmsAiDashboardApplication.class, args);
+    }
+}
